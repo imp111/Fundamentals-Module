17289cdcca6394abc275d62828900c0ef63f4dc7
imp111 <alexsandartenev@gmail.com>
2021-03-25 14:01:59 +0200

    First commit

---
+++ 07. Methods - Lab/07. Methods - Lab/multiply.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Methods___Lab
+{
+    class multiply
+    {
+        public static void Palindrome(int input)
+        {
+           
+        }
+
+        private static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
+
+
