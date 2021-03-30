d619143e809aacce54242f01a145c2b372358bcb
imp111 <alexsandartenev@gmail.com>
2021-03-30 14:59:46 +0300

    OOP exercises

---
+++ 10. OOP - Lab/06. Store Boxes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Store_Boxes
+{
+    class Item
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    class Box
+    {
+        public int SerialNumber { get; set; }
+        public Item Item { get; set; }
+        public int ItemQuantity { get; set; }
+        public decimal PriceBox { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+
+            }
+        }
+    }
+}
