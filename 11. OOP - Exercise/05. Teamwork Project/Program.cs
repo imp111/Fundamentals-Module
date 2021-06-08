db9e9367374cb8b3c530b227197c10d11d058628
imp111 <alexsandartenev@gmail.com>
2021-06-08 22:23:11 +0300

    bitwise operations added

--- 11. OOP - Exercise/05. Teamwork Project/Program.cs
+++ 11. OOP - Exercise/05. Teamwork Project/Program.cs
@@ -10,6 +10,14 @@ namespace _05.Teamwork_Projects
     {
         public static void Main(string[] args)
         {
+            int numberOfTeams = int.Parse(Console.ReadLine());
+
+            string initialInput = Console.ReadLine();
+
+            while (initialInput != "end of assigment")
+            {
+                string[] input = initialInput.Split('-').ToArray();
+            }
         }
     }
 }
