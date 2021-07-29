177df0d44326206f8b20c61086f19d6cd5945530
Aleksandar Tenev <alexsandartenev@gmail.com>
2021-07-29 13:32:11 +0300

    ex 03, 04 added

---
+++ 12. Associative Arrays - Lab/03. Word Synonyms/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new Dictionary<string, List<string>>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (words.ContainsKey(word) == false)
+                {
+                    words.Add(word, new List<string>());
+                }
+
+                words[word].Add(synonym);
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word.Key} - {word.Value}");
+            }
+        }
+    }
+}
